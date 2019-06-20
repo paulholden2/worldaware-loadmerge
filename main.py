17c12ae0303d4ac0cb9061a802403f6759a34901
Paul Holden <pholden@stria.com>
2019-06-19 17:02:32 -0700

    Can process multiple output directories

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import re
 import csv
 import codecs
 
-output_dir = '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\Output\\Client'
+output_dirs = [
+    '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\Output\\Client',
+    '\\\\stria-prod1\\CID01570 - WorldAware\\JID01215 - CaaS\\Output\\Partner',
+]
 
 id_column = 'Document Path'
 
@@ -63,33 +66,37 @@ def scrub_load_row(row):
 
     return row
 
-for dir in os.listdir(output_dir):
-    delivery_dir = os.path.join(output_dir, dir)
-    load_file_path = os.path.join(delivery_dir, 'Ingestion Load File.csv')
-    out_load_file_path = os.path.join(delivery_dir, 'Processed Ingestion Load File.csv')
-    if not os.path.isfile(load_file_path):
-        print('No load file found for: %s' % dir)
-        continue
+def process_output_dir(output_dir):
+    for dir in os.listdir(output_dir):
+        delivery_dir = os.path.join(output_dir, dir)
+        load_file_path = os.path.join(delivery_dir, 'Ingestion Load File.csv')
+        out_load_file_path = os.path.join(delivery_dir, 'Processed Ingestion Load File.csv')
+        if not os.path.isfile(load_file_path):
+            print('No load file found for: %s' % dir)
+            continue
 
-    last_id = None
-    last_row = None
-    row_data = []
-    # Populate row_data with data
-    with codecs.open(load_file_path, 'r', encoding='utf-8-sig') as csv_file:
-        csv_reader = UnicodeCsvDictReader(csv_file)
-        for row in csv_reader:
-            if last_id == row[id_column]:
-                for col in merge_columns:
-                    new_val = row[col]
-                    if new_val is not None and new_val != '' and new_val != 'NOT NEEDED':
-                        last_row[col] += '|' + row[col]
-            else:
-                last_row = row.copy()
-                row_data.append(last_row)
-                last_id = row[id_column]
+        last_id = None
+        last_row = None
+        row_data = []
+        # Populate row_data with data
+        with codecs.open(load_file_path, 'r', encoding='utf-8-sig') as csv_file:
+            csv_reader = UnicodeCsvDictReader(csv_file)
+            for row in csv_reader:
+                if last_id == row[id_column]:
+                    for col in merge_columns:
+                        new_val = row[col]
+                        if new_val is not None and new_val != '' and new_val != 'NOT NEEDED':
+                            last_row[col] += '|' + row[col]
+                else:
+                    last_row = row.copy()
+                    row_data.append(last_row)
+                    last_id = row[id_column]
 
-        with open(out_load_file_path, 'wb') as csv_file:
-            csv_writer = csv.DictWriter(f=csv_file, fieldnames=csv_reader.fieldnames)
-            csv_writer.writeheader()
-            for row in row_data:
-                csv_writer.writerow(scrub_load_row(row))
+            with open(out_load_file_path, 'wb') as csv_file:
+                csv_writer = csv.DictWriter(f=csv_file, fieldnames=csv_reader.fieldnames)
+                csv_writer.writeheader()
+                for row in row_data:
+                    csv_writer.writerow(scrub_load_row(row))
+
+for dir in output_dirs:
+    process_output_dir(dir)
